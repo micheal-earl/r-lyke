f3a06f4928e50e02b3416cde7b056af1d1cb9760
Zenrix <zenrixmike@gmail.com>
2015-06-30 16:57:58 -0700

    Now accepts input, can move player with WASD. Collision detection against walls.

--- input.rb
+++ input.rb
@@ -1,12 +1,28 @@
 require 'ncurses'
 
+KEY_W = 119
+KEY_A = 97
+KEY_S = 115
+KEY_D = 100
+
 begin
   Ncurses.initscr
 
   while(true)
     ch = Ncurses.getch
-    if ch == "w"
-      Ncurses.mvaddstr(0, 0, "I'm w!")
+    case ch
+    when KEY_W
+      Ncurses.mvaddstr(0, 0, "UP")
+      Ncurses.refresh
+    when KEY_S
+      Ncurses.mvaddstr(0, 0, "DOWN")
+      Ncurses.refresh
+    when KEY_A
+      Ncurses.mvaddstr(0, 0, "LEFT")
+      Ncurses.refresh
+    when KEY_D
+      Ncurses.mvaddstr(0, 0, "RIGHT")
+      Ncurses.refresh
     end
   end
 
--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require "ncurses"
 
+KEY_W = 119
+KEY_A = 97
+KEY_S = 115
+KEY_D = 100
+KEY_POUND = 35
+KEY_SPACE = 32
+KEY_AT = 64
+
 game_map = [  "###############",
               "#      #      #",
               "#             #",
@@ -14,30 +22,43 @@ game_map = [  "###############",
 begin
   # Initialize Curse
   Ncurses.initscr
+  # Hide terminal cursor
+  Ncurses.curs_set(0)
 
-  # Draw game map
-  for y in 0...10
-    for x in 0...15
-      if(game_map[y][x] == "#")
-        Ncurses.mvaddch(y, x, 35)
-      else
-        Ncurses.mvaddch(y, x, 32)
-      end
-    end
-  end
+  mover = 0
+  debug = 0 #Used for debugging purposes if you didn't guess that
 
-  # Player's starting coordinates
+  # Players starting coords
   y = 1
   x = 1
 
-  # Draw player
-  Ncurses.mvaddch(y, x, 64);
+  while mover != 27
+    # Draw game map
+    for yy in 0...10
+      for xx in 0...15
+        if(game_map[yy][xx] == "#")
+          Ncurses.mvaddch(yy, xx, KEY_POUND)
+        else
+          Ncurses.mvaddch(yy, xx, KEY_SPACE)
+        end
+      end
+    end
 
-  # Wait for key press before leaving
-  Ncurses.refresh
-  Ncurses.getch
-  sleep 1000
+    y -= 1 if mover == KEY_W && game_map[y-1][x] == " "
+    y += 1 if mover == KEY_S && game_map[y+1][x] == " "
+    x -= 1 if mover == KEY_A && game_map[y][x-1] == " "
+    x += 1 if mover == KEY_D && game_map[y][x+1] == " "
 
+    # Some Debugging stuff
+    debug += 1
+    Ncurses.mvaddstr(15, 15, "#{x}, #{y}, #{mover} mover #{debug} #{game_map[y-1][x]} #{game_map[y-1][x] == " "}")
+
+    # Redraw player
+    Ncurses.mvaddch(y, x, KEY_AT);
+
+    Ncurses.refresh
+    mover = Ncurses.getch
+  end
 ensure
   Ncurses.endwin
 end
