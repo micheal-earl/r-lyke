7c238a7f9112ac5582284dabb3d4f0dd8f193e26
Zenrix <zenrixmike@gmail.com>
2015-07-02 18:04:35 -0700

    Refactored some stuff

--- main.rb
+++ main.rb
@@ -3,20 +3,20 @@ require "./input.rb"
 require "./mobiles.rb"
 
 class Game
-  @KEY_W = 119
-  @KEY_A = 97
-  @KEY_S = 115
-  @KEY_D = 100
-  @KEY_POUND = 35
-  @KEY_SPACE = 32
-  @KEY_AT = 64
-  @KEY_ESC = 27
+  def initialize
+    @key = Hash['W'     => 119, 'A'    => 97, 'S'  => 115, 'D'  => 100,
+                'POUND' => 35, 'SPACE' => 32, 'AT' => 64, 'ESC' => 27
+                'Y'     => 121]
+  end
 
   def generate_map(size)
     map_array = []
 
     (rand(size) + 4).times do |n|
-      map_array[n] = "#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}"
+      map_array[n] = "#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}"\
+                     "#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}"\
+                     "#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}"\
+                     "#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}#{rand(3)}"
     end
 
     map_array.each_index do |n|
@@ -43,7 +43,6 @@ class Game
   end
 
   def game_loop(scr)
-
     scr.clear
     game_map = generate_map(15)
 
@@ -51,60 +50,58 @@ class Game
     y = 1
     x = 1
 
-    # Key listener?
-    mover = 0
-
+    mover = 0 # Initalize variable to hold keyboard input
     debug = 0 # Used for debugging purposes if you didn't guess that
 
-    while mover != 27
-      # Draw game map
-      scr.clear
+    while mover != @key['ESC']
+      scr.clear # Clear the screen from previous iteration
+
+      # Draw the game map
       for rows in 0..game_map.length
         scr.mvaddstr(rows, 0, game_map[rows].to_s)
       end
 
       # Listen for input and modify player coords
-      y -= 1 if mover == 119 && game_map[y-1][x] == " "
-      y += 1 if mover == 115 && game_map[y+1][x] == " "
-      x -= 1 if mover == 97  && game_map[y][x-1] == " "
-      x += 1 if mover == 100 && game_map[y][x+1] == " "
-      game_loop(scr) if mover == 32
+      y -= 1 if mover == @key['W'] && game_map[y-1][x] == " "
+      y += 1 if mover == @key['S'] && game_map[y+1][x] == " "
+      x -= 1 if mover == @key['A'] && game_map[y][x-1] == " "
+      x += 1 if mover == @key['D'] && game_map[y][x+1] == " "
+      game_loop(scr) if mover == @key['SPACE']
 
-      # Redraw player
-      scr.mvaddch(y, x, 64);
+      scr.mvaddch(y, x, @key['AT']); # Redraw player
 
-      # Some Debugging stuff
+      # ==================== DEBUG BLOCK BEGIN ====================
       debug += 1
       scr.mvaddstr(2, 25, "WASD to move, ESC to exit,")
       scr.mvaddstr(3, 25, "SPACE to generate new map")
-      scr.mvaddstr(4, 25, "#{x}, #{y}, #{mover} mover #{debug} #{game_map[y-1][x]} #{game_map[y-1][x] == " "}")
+      scr.mvaddstr(4, 25, "#{x}, #{y}, #{mover} mover #{debug}"\
+                          " #{game_map[y-1][x]} #{game_map[y-1][x] == " "}")
+      # ===================== DEBUG BLOCK END =====================
 
-      # Refresh screen
-      scr.refresh
-
-      # Get char
-      mover = scr.getch
+      scr.refresh       # Refresh screen
+      mover = scr.getch # Get user input for next frame
     end
   end
 
   def init_scr
     begin
-      # initialize ncurses
-      Ncurses.initscr
-      Ncurses.curs_set(0)      # Hide cursor
-      Ncurses.cbreak           # provide unbuffered input
-      Ncurses.noecho           # turn off input echoing
-      Ncurses.nonl             # turn off newline translation
-      Ncurses.stdscr.intrflush(false) # turn off flush-on-interrupt
-      Ncurses.stdscr.keypad(true)     # turn on keypad mode
+      Ncurses.initscr                 # Initialize ncurses
+      Ncurses.curs_set(0)             # Hide cursor
+      Ncurses.cbreak                  # Provide unbuffered input
+      Ncurses.noecho                  # Turn off input echoing
+      Ncurses.nonl                    # Turn off newline translation
+      Ncurses.stdscr.intrflush(false) # Turn off flush-on-interrupt
+      Ncurses.stdscr.keypad(true)     # Turn on keypad mode
 
-      Ncurses.stdscr.mvaddstr(10, 35, "Want to play?") # output string
-      Ncurses.stdscr.mvaddstr(11, 35, "    Y or N   ") # output string
-      #Ncurses.stdscr.getch                             # get a charachter
+      # Print string to the screen
+      Ncurses.stdscr.mvaddstr(10, 35, "Want to play?")
+      Ncurses.stdscr.mvaddstr(11, 35, "    Y or N   ")
 
-      game_loop(Ncurses.stdscr) if Ncurses.stdscr.getch == 121 # demo of moving cursor
+      # Start the game_loop if user presses Y
+      game_loop(Ncurses.stdscr) if Ncurses.stdscr.getch == @key['Y']
 
     ensure
+      # Clean up everything when we're done using ncurses
       Ncurses.echo
       Ncurses.nocbreak
       Ncurses.nl
@@ -115,4 +112,4 @@ end
 
 rlyke = Game.new
 
-puts rlyke.init_scr
+rlyke.init_scr
